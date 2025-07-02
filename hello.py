77c8e506f2bba39b28114f3c53a7283025b7ba13
Lokesh <d.lokesh121@gmail.com>
2025-07-02 07:58:01 -0500

    added git ignore

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("hello.. this is my first time using Pyton. exciting!!!")
+x=2
+print(x)
+x=x+4
+print(x)
+hrs = input("Enter hrs: ")
+rate = input("Enter rate: ")
+fh = float(hrs)
+pay = float(hrs) * float(rate)
+print("pay: ", pay)
